38a05218ab80ce94ccfd1f54ad31e1bf90bd5435
Javier19-cmd <63107636+Javier19-cmd@users.noreply.github.com>
2021-05-11 17:47:21 -0600

    agregando algunos métodos al árbol

--- SplayTree.java
+++ SplayTree.java
@@ -3,12 +3,13 @@
  * @see https://algs4.cs.princeton.edu/33balanced/SplayBST.java.html
  */
 public class SplayTree <Key extends Comparable <Key>, Value> implements TInterfaz<Key, Value>{
+    
+    public Node root;   // Raíz para el SplayTree
+    
     /** Class Node **/
 
   private class Node {
 
-
-    public Node root;   // Raíz para el SplayTree
     
     // Nodo que será de ayuda para poder trabajar los valores del árbol. 
     
@@ -20,11 +21,6 @@ public class SplayTree <Key extends Comparable <Key>, Value> implements TInterfa
     public Node(Key key, Value value) {
         this.key   = key;
         this.value = value;
-    }
-    
-    //Método que se encarga de verificar si existe o no el valor a buscar.
-    public boolean contains(Key key) {
-        return get(key) != null;
     }
     
     //Retorno del valor asociado con su correspondiente llave.
@@ -54,14 +50,47 @@ public class SplayTree <Key extends Comparable <Key>, Value> implements TInterfa
     */
     @Override
     public void put(Key key, Value value) {
-        // TODO Auto-generated method stub
+        
+        if(root == null){//Si la raíz está vacía, entonces se crea una nueva raíz usando la clase de Nodo.
+
+            root = new Node(key, value); //Creando la raíz.
+            return;
+        }
+
+        root = splay(root, key); //Usando el método splay para la raíz.
+
+        int cmp = key.compareTo(root.key); //Comparando la llave con la raíz.
+
+        //Insertando un nuevo nodo en la parte izquierda si en caso la comparación es menor a cero.
+        if(cmp < 0){
+            
+            //Insertando el nodo a la izquierda del árbol.
+            Node n = new Node(key, value);
+            n.left = root.left; 
+            n.right = root; 
+            root.left = null; 
+            root = n; 
+        
+        //Insertando un nuevo nodo en la parte derecha, en caso la comparación es mayor a cero.    
+        }else if(cmp > 0){
+
+            //Insertando el nodo a la derecha del árbol.
+            Node n = new Node(key, value); //Creando el nodo.
+            n.right = root.right;
+            root.left = null; 
+            root = n; 
+        
+        }else{//Si en caso hay un valor duplicado, entonces se reemplaza ese valor en donde sea que esté.
+
+            root.value = value; 
+        }
         
     }
 
+    //Método que se encarga de verificar si existe o no el valor a buscar.
     @Override
     public boolean contains(Key key) {
-        // TODO Auto-generated method stub
-        return false;
+        return get(key) != null; //Retornando el valor a buscar.
     }
 
     @Override
